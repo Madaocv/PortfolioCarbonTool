d82db0ad0de3a171cc82a9d329b1bd87bdd30963
mateovelmontes <oleg.eliiashiv@cloudcod.com>
2024-09-11 23:43:40 +0300

    omg

--- app/settings.py
+++ app/settings.py
@@ -131,13 +131,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'  # Це коректно, залишається без змін
+
+# STATIC_ROOT - це папка, куди будуть зібрані всі статичні файли після команди collectstatic.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_DIRS - це список директорій, в яких Django шукає вихідні файли статичних ресурсів (CSS, JS, зображення)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
