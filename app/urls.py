46973dd2893be0a87faf58761839df9cb54cb40e
mateovelmontes <oleg.eliiashiv@cloudcod.com>
2024-09-16 00:58:29 +0300

    utils on the base

--- app/urls.py
+++ app/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('', include('uploads_data.urls')),
     path('', include('calculations_and_pages.urls')), 
 ]
-# if settings.DEBUG:
+if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
